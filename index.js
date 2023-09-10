b5fcd81cb5dad2f74b5baeff272dfc5728e61431
Shahrear002 <shahrear002@gmail.com>
2023-09-10 17:51:34 +0600

    passport-jwt authentication strategy added'

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
 const user = require('./routes/users');
 
@@ -10,6 +11,9 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(passport.initialize())
+require('./config/passport')(passport)
 
 mongoose
     .connect(process.env.DB_CONNECT, {
